193a22428b8192342a40d15ee95efbd074169669
Youngjae Lim <limyoungjae@gmail.com>
2022-04-14 13:40:17 -0400

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path')
+
+const postCSSPlugins = [
+  require('postcss-simple-vars'),
+  require('postcss-nested'),
+  require('autoprefixer'),
+]
+
+module.exports = {
+  entry: './app/assets/scripts/App.js',
+  output: {
+    filename: 'bundled.js',
+    path: path.resolve(__dirname, 'app'),
+  },
+  devServer: {
+    watchFiles: ['./app/**/*.html'],
+    static: {
+      directory: path.join(__dirname, 'app'),
+    },
+    hot: true,
+    port: 3000,
+    host: '0.0.0.0',
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: { url: false },
+          },
+          {
+            loader: 'postcss-loader',
+            options: { postcssOptions: { plugins: postCSSPlugins } },
+          },
+        ],
+      },
+    ],
+  },
+}
